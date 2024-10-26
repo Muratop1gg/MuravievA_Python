ea5b3d76b0068ffeecd57b2e53cbd2b3cb89e85c
Andrey Muraviev <97607096+Muratop1gg@users.noreply.github.com>
2024-10-26 21:35:02 +0300

    Deleted: Лабораторная и самостоятельная работы 2/Самостоятельная работа 2 directory

--- Лабораторная и самостоятельная работы 2/Самостоятельная работа 2/task1.py
+++
@@ -1,9 +0,0 @@
-src = not False and True or False and not True
-
-# True and True or False and False
-# True or False
-# True
-
-result = True
-
-print(src == result)
--- Лабораторная и самостоятельная работы 2/Самостоятельная работа 2/task4.py
+++
@@ -1,6 +0,0 @@
-res = 1
-
-for i in range(1, 11):
-    res *= i
-
-print(res)
